f4fb9c4d6263a70653121ecc36ad3699c2c6aaa8
Alison Williams <alison.lauriewill@gmail.com>
2019-06-12 10:34:12 +0100

    Guest has name test passed

---
+++ src/test/java/GuestTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class GuestTest {
+
+    Guest guest;
+
+    @Before
+    public void before(){
+        guest = new Guest("String");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("String", guest.getName());
+    }
+    
+}
